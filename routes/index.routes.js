4abe953f2614f0b5b6e7b11187bdd8e8c8c1b190
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-08-13 14:39:16 +0200

    feat: add health check

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+// GET /api/health
+router.get("/health", (req, res, next) => {
+  // send ping to prevent inactivity on mongodb atlas
+  mongoose.connection.db
+    .admin()
+    .ping()
+    .then(() => {
+      res
+        .status(200)
+        .json({ status: "ok", timestamp: new Date().toISOString() });
+    })
+    .catch((err) => {
+      console.error("MongoDB ping failed:", err);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to connect to MongoDB",
+      });
+    });
+});
+
 const userRoutes = require("./user.routes");
 router.use("/", userRoutes);
 
@@ -15,8 +37,5 @@ router.use("/", invitationRoutes);
 
 const commentRoutes = require("./comment.routes.js");
 router.use("/", commentRoutes);
-
-
-
 
 module.exports = router;
